7f953698cc7bf63b7f07c9b92a3e2891978a61f8
Brighten Tompkins <brightenqtompkins@gmail.com>
2023-03-17 05:34:20 -0700

    :bug: (naming) brighten_light->brighten-light

--- colors/brighten_light.lua
+++ colors/brighten_light.lua
@@ -1,6 +1,6 @@
 -- You probably always want to set this in your vim file
 vim.opt.background = 'dark'
-vim.g.colors_name = 'brighten_light'
+vim.g.colors_name = 'brighten-light'
 
 -- By setting our module to nil, we clear lua's cache,
 -- which means the require ahead will *always* occur.
